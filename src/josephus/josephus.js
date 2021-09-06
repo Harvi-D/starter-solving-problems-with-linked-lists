7e9b436326ae3b6862ad8a9283b8cd23007f3b47
Dylan Harville <dr.dyloh@gmail.com>
2021-09-06 15:09:09 -0600

    all tests passing

--- src/josephus/josephus.js
+++ src/josephus/josephus.js
@@ -1,3 +1,30 @@
-function josephus(list, m) {}
+const LinkedList = require('../lib/linkedList');
+const tester = new LinkedList;
+tester.insertAtHead('D');
+tester.insertAtHead('C');
+tester.insertAtHead('B');
+tester.insertAtHead('A');
+
+
+function josephus(list, m) {
+    let node = list.head;
+    const tail = list.find((node) => !node.next);
+    tail.next = list.head;
+
+    while (node.next != node) {
+        for (let i = 1; i < m; i++) {
+            node = node.next;
+            console.log(node);
+        }
+        if (node.next === list.head) {
+            list.head = list.head.next;
+        }
+        node.next = node.next.next;
+        node = node.next;
+        console.log(node);
+    }
+    return node.value;
+}
+console.log(josephus(tester, 3));
 
 module.exports = josephus;
